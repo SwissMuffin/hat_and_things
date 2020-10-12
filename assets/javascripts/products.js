371675ca3ad67af9f52bb373a0a46faf1dc98d0d
SwissMuffin <gillieron.victor@gmail.com>
2020-10-12 17:08:23 +0200

    first task - Defining a simple JS object

---
+++ assets/javascripts/products.js
@@ -0,0 +1,19 @@
+/*    1. Define a simple JavaScript object that represents a hat, with properties name,
+ price, color, and imageHref.*/
+
+let Hat = {
+    name:"<name>",
+    price:"<price>",
+    color:"<color>",
+    imageHref:"<imageHerf>"
+};
+
+   /* 2. Attach a function toString() to the object that will
+return a String that nicely formats the properties of the hat object i.e. "<name>,
+ color <color>, price: <price>, image: <imageHref>". You can test this object using
+ console.log().
+*/
+
+
+
+
