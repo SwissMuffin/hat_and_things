91dd214fad5302251bf4b48289aa68b14f737a85
swissmuffin <gillieron.victor@gmail.com>
2021-01-05 14:17:09 +0100

    display accessories has been modified do accomodate changes dinamically

--- assets/javascripts/products.js
+++ assets/javascripts/products.js
@@ -1,13 +1,13 @@
-/*------------------------------------------HATS--------------------------------------------------------*/
+/*------------------------------------------accessoryS--------------------------------------------------------*/
 
-const hat = {
+const accessory = {
     name: "<name>",
     price: "<price>",
     color: "<color>",
     imageHref: "<imageHerf>"
 };
 
-hat.toString = function () {
+accessory.toString = function () {
     return `name ${this.name} price ${this.price} color ${this.color} imageHref ${this.imageHref}`
 }
 
@@ -22,80 +22,80 @@ function Accessory(name, price, color, imageHref) {
 
 /* --------------------------------------------TASK 03-----------------------------------------------------*/
 
-/*const hatArray = [
-    product = new Accessory("Baseball cap","11.99", "red", "whatever"),
-    product = new Accessory("Baseball cap", "11.99", "blue", "whatever" ),
-    product = new Accessory("Baseball cap", "11.99", "yellow", "whatever"),
-    product = new Accessory("Baseball cap", "11.99", "green", "whatever"),
-    product = new Accessory("Beanie", "17.99", "red", "whatever"),
-    product = new Accessory("Beanie", "17.99", "blue", "whatever"),
-    product = new Accessory("Beanie", "17.99", "green", "whatever"),
-    product = new Accessory("Straw hat", "10.99", "green", "whatever"),
-    product = new Accessory("Straw hat", "10.99", "yellow", "whatever"),
-    product = new Accessory("Trilby", "10.99", "red", "whatever"),
-    product = new Accessory("Trilby", "10.99", "blue", "whatever"),
-    product = new Accessory("Trilby", "10.99", "yellow", "whatever"),
-    product = new Accessory("Trilby", "10.99", "blue", "whatever"),
-    product = new Accessory("Trilby", "10.99", "blue", "whatever")
+/*const accessoryArray = [
+    product = new Accessory("Baseball cap","11.99", "red", "waccessoryever"),
+    product = new Accessory("Baseball cap", "11.99", "blue", "waccessoryever" ),
+    product = new Accessory("Baseball cap", "11.99", "yellow", "waccessoryever"),
+    product = new Accessory("Baseball cap", "11.99", "green", "waccessoryever"),
+    product = new Accessory("Beanie", "17.99", "red", "waccessoryever"),
+    product = new Accessory("Beanie", "17.99", "blue", "waccessoryever"),
+    product = new Accessory("Beanie", "17.99", "green", "waccessoryever"),
+    product = new Accessory("Straw accessory", "10.99", "green", "waccessoryever"),
+    product = new Accessory("Straw accessory", "10.99", "yellow", "waccessoryever"),
+    product = new Accessory("Trilby", "10.99", "red", "waccessoryever"),
+    product = new Accessory("Trilby", "10.99", "blue", "waccessoryever"),
+    product = new Accessory("Trilby", "10.99", "yellow", "waccessoryever"),
+    product = new Accessory("Trilby", "10.99", "blue", "waccessoryever"),
+    product = new Accessory("Trilby", "10.99", "blue", "waccessoryever")
 ];*/
 
 /*---------------------------------------------TASK 04-----------------------------------------------------*/
 
-const allHats = document.querySelectorAll(".accessory");
+const allaccessorys = document.querySelectorAll(".accessory");
 
-const hatArr = [];
-   for (let i = 0; i < allHats.length; i++) {
-       const valName = allHats[i].children[0].children[2].children[0].textContent;
-       const valPrice = allHats[i].children[0].children[0].textContent;
-       const valColor = allHats[i].children[0].children[2].children[1].children[0].textContent;
-       const valImageHref = allHats[i].children[0].children[1].src;
-       hatArr.push(new Accessory(`${valName}`, `${valPrice}`, `${valColor}`, `${valImageHref}`));// each iteration creates a new object according to the hat Prototype.
+const accessoryArr = [];
+   for (let i = 0; i < allaccessorys.length; i++) {
+       const valName = allaccessorys[i].children[0].children[2].children[0].textContent;
+       const valPrice = allaccessorys[i].children[0].children[0].textContent;
+       const valColor = allaccessorys[i].children[0].children[2].children[1].children[0].textContent;
+       const valImageHref = allaccessorys[i].children[0].children[1].src;
+       accessoryArr.push(new Accessory(`${valName}`, `${valPrice}`, `${valColor}`, `${valImageHref}`));// each iteration creates a new object according to the accessory Prototype.
     }
 
-    function displayHat (aHat) {
+    function displayAccessory (anAccessory) {
 
-        const hatPrice = document.createElement("div");
-        hatPrice.className = "currency btn btn-light disabled";
-        hatPrice.textContent = aHat.price;
+        const accessoryPrice = document.createElement("div");
+        accessoryPrice.className = "currency btn btn-light disabled";
+        accessoryPrice.textContent = anAccessory.price;
 
-        const hatImage = document.createElement("img");
-        hatImage.className = "card-img-top";
-        hatImage.src = "./assets/images/red/hats/2.png" // static for demonstration purposes.
+        const accessoryImage = document.createElement("img");
+        accessoryImage.className = "card-img-top";
+        accessoryImage.src = anAccessory.imageHref;
 
-        const hatName = document.createElement("h5");
-        hatName.className = "card-title";
-        hatName.textContent = aHat.name;
+        const accessoryName = document.createElement("h5");
+        accessoryName.className = "card-title";
+        accessoryName.textContent = anAccessory.name;
 
-        const hatColor = document.createElement("p");
-        hatColor.className = "card-text";
-        hatColor.textContent = "Color: ";
+        const accessoryColor = document.createElement("p");
+        accessoryColor.className = "card-text";
+        accessoryColor.textContent = "Color: ";
 
-        const hatColorValue = document.createElement("em");
-        hatColorValue.className = "card-text";
-        hatColorValue.textContent = aHat.color; // This will be de additional class to be created for the filter
-        hatColor.appendChild(hatColorValue);
+        const accessoryColorValue = document.createElement("em");
+        accessoryColorValue.className = "card-text";
+        accessoryColorValue.textContent = anAccessory.color; // This will be de additional class to be created for the filter
+        accessoryColor.appendChild(accessoryColorValue);
 
-        const hatButton = document.createElement("button");
-        hatButton.className = "btn btn-outline-primary";
-        hatButton.textContent = "Add to wishlist!";
+        const accessoryButton = document.createElement("button");
+        accessoryButton.className = "btn btn-outline-primary";
+        accessoryButton.textContent = "Add to wishlist!";
 
-        const hatBody = document.createElement("div");
-        hatBody.className = "card-body text-center";
-        hatBody.appendChild(hatName);
-        hatBody.appendChild(hatColor);
-        hatBody.appendChild(hatButton);
+        const accessoryBody = document.createElement("div");
+        accessoryBody.className = "card-body text-center";
+        accessoryBody.appendChild(accessoryName);
+        accessoryBody.appendChild(accessoryColor);
+        accessoryBody.appendChild(accessoryButton);
 
-        const hatCard = document.createElement("div");
-        hatCard.className = "card my-3";
-        hatCard.appendChild(hatPrice);
-        hatCard.appendChild(hatImage);
-        hatCard.appendChild(hatBody);
+        const accessoryCard = document.createElement("div");
+        accessoryCard.className = "card my-3";
+        accessoryCard.appendChild(accessoryPrice);
+        accessoryCard.appendChild(accessoryImage);
+        accessoryCard.appendChild(accessoryBody);
 
-        const hatAccessory = document.createElement("div");
-        hatAccessory.className = "accessory col-sm-4";
-        hatAccessory.appendChild(hatCard);
+        const accessoryAccessory = document.createElement("div");
+        accessoryAccessory.className = "accessory col-sm-4";
+        accessoryAccessory.appendChild(accessoryCard);
 
-        document.querySelector("#products").appendChild(hatAccessory);
+        document.querySelector("#products").appendChild(accessoryAccessory);
 }
 
 /*-------------------------------------FILTERS BY COLOR----------------------------------------------------------------*/
@@ -109,9 +109,9 @@ function highlightSelectedFilter() {
         function activateFilter(e) {
             e.preventDefault();
             activeFilter[i].setAttribute("class", "btn btn-outline-secondary active");
-            for (let i = 0; i < allHats.length; i++) {
-                if (hatArr[i]["color"].toLowerCase() === e.target.textContent.toLowerCase()) { //all hat color buttons match
-                    allHats[i].setAttribute("class", "accessory col-sm-4" + ` ${hatArr[i]["color"].toLowerCase()}`);
+            for (let i = 0; i < allaccessorys.length; i++) {
+                if (accessoryArr[i]["color"].toLowerCase() === e.target.textContent.toLowerCase()) { //all accessory color buttons match
+                    allaccessorys[i].setAttribute("class", "accessory col-sm-4" + ` ${accessoryArr[i]["color"].toLowerCase()}`);
                 }
             }
         }
@@ -119,57 +119,64 @@ function highlightSelectedFilter() {
 
 }
 
-function filterHatsByColor(e) {
+function filteraccessorysByColor(e) {
 
-    for (let i = 0; i < allHats.length; i++) {
-        allHats[i].style.display = "none";
-        if (hatArr[i]["color"].toLowerCase() === e.target.textContent.toLowerCase()) {
-            allHats[i].style.display = "block";
+    for (let i = 0; i < allaccessorys.length; i++) {
+        allaccessorys[i].style.display = "none";
+        if (accessoryArr[i]["color"].toLowerCase() === e.target.textContent.toLowerCase()) {
+            allaccessorys[i].style.display = "block";
         }
     }
 
-    for (let i = 0; i < allHats.length; i++) {
+    for (let i = 0; i < allaccessorys.length; i++) {
         if (e.target.textContent.toLowerCase() === "all") {
-            allHats[i].style.display = "block";
+            allaccessorys[i].style.display = "block";
         }
     }
 }
  const btnRed = document.querySelector(".btn-group").children[0];
     btnRed.addEventListener("click", highlightSelectedFilter, false);
-    btnRed.addEventListener("click", filterHatsByColor, false);
+    btnRed.addEventListener("click", filteraccessorysByColor, false);
 
  const btnBlue = document.querySelector(".btn-group").children[1];
     btnBlue.addEventListener("click", highlightSelectedFilter, false);
-    btnBlue.addEventListener("click", filterHatsByColor, false);
+    btnBlue.addEventListener("click", filteraccessorysByColor, false);
 
  const btnGreen = document.querySelector(".btn-group").children[2];
     btnGreen.addEventListener("click", highlightSelectedFilter, false);
-    btnGreen.addEventListener("click", filterHatsByColor, false);
+    btnGreen.addEventListener("click", filteraccessorysByColor, false);
 
  const btnYellow = document.querySelector(".btn-group").children[3];
     btnYellow.addEventListener("click", highlightSelectedFilter, false);
-    btnYellow.addEventListener("click", filterHatsByColor, false);
+    btnYellow.addEventListener("click", filteraccessorysByColor, false);
 
  const btnAll = document.querySelector(".btn-group").children[4];
     btnAll.addEventListener("click", highlightSelectedFilter, false);
-    btnAll.addEventListener("click", filterHatsByColor, false);
+    btnAll.addEventListener("click", filteraccessorysByColor, false);
 
-/* --------------------- Btn Hats, Socks, Sunglasses--------------------------------- */
+/* --------------------- Btn accessorys, Socks, Sunglasses--------------------------------- */
 
-const btnNavHat = document.querySelector(".navbar-nav").children[0];
-    btnNavHat.addEventListener("click", loadRemoteAccessories, false);
+const btnNavaccessory = document.querySelector(".navbar-nav").children[0];
+    btnNavaccessory.addEventListener("click", loadRemoteAccessories, false);
 const btnNavSocks = document.querySelector(".navbar-nav").children[1];
     btnNavSocks.addEventListener("click", loadRemoteAccessories, false);
 const btnNavSunglasses = document.querySelector(".navbar-nav").children[2];
     btnNavSunglasses.addEventListener("click", loadRemoteAccessories, false);
 
 function loadRemoteAccessories(e) {
+    //console.log(accessoryArr);
+    for ( let i = 0; i < allaccessorys.length; i++) {
+        allaccessorys[i].style.display = "none";
+    }
+
+
+
     switch (e.target.textContent.toLocaleLowerCase()) {
 
-        case "hats":
+        case "accessorys":
 
-            let hatRequest = new XMLHttpRequest();
-            hatRequest.open('GET', '' )
+            let accessoryRequest = new XMLHttpRequest();
+            accessoryRequest.open('GET', '' )
             //console.log(e.target.textContent);
             break;
 
@@ -178,19 +185,15 @@ function loadRemoteAccessories(e) {
             let socksRequest = new XMLHttpRequest();
             socksRequest.open('GET', './socks.json', false );
             socksRequest.onload = () => {
-                //console.log(socksRequest.responseText);
                 let socksArray = JSON.parse(socksRequest.responseText);
-                for (let i = 0; i < socksArray.length; i++) {
-                   if (e.target.textContent.toLowerCase() === socksArray[i]["name"].toLowerCase()) {
-                       console.log("I am here");
-                   }
-
+                for ( let i = 0; i < socksArray.length; i++) {
+                    displayAccessory(socksArray[i]);
                 }
+
+
 
             }
             socksRequest.send();
-            //
-            // console.log("Socks");
             break;
 
         case "sunglasses":
@@ -198,10 +201,13 @@ function loadRemoteAccessories(e) {
             let sunglassesRequest = new XMLHttpRequest();
             sunglassesRequest.open('GET', './sunglasses.json', false );
             sunglassesRequest.onload = () => {
-                console.log(sunglassesRequest.responseText)
+                let sunglassesSet = JSON.parse(sunglassesRequest.responseText);
+                for (let i = 0; i < sunglassesSet.length; i++){
+                    displayAccessory(sunglassesSet[i]);
+                }
             }
             sunglassesRequest.send();
-            // console.log("Sunglasses");
+
             break;
 
     }
